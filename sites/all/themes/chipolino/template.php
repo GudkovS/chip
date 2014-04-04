f9feb4823f8925268cdcf84cee08a7c7815fa76e
GudkovS <gudkov.sergiy@gmail.com>
2014-04-04 12:29:13 +0300

    add module: field_multiple_limit, field_formatter_settings; update custom module: chip

--- sites/all/themes/chipolino/template.php
+++ sites/all/themes/chipolino/template.php
@@ -1,1 +1,21 @@
 <?php
+
+/**
+ * Implements template_preprocess_field().
+ */
+function chipolino_preprocess_field(&$vars) {
+
+   // dpm($vars);
+
+    /*
+    if ($vars['element']['#entity_type'] == 'field_collection_item') {
+        unset($vars);
+    }
+    */
+
+    /*
+     if ($vars['element']['#field_name'] == 'field_my_custom_field') {
+        $vars['items'][0]['#markup'] = "I changed the output of my field!";
+    }
+    */
+}
